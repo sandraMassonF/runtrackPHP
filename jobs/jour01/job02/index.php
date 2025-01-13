d6c8747c92c0a34537c4fdc16a4c9c826ba37537
sandraMasson-F <sandra.bacot@laplateforme.io>
2025-01-13 11:23:16 +0100

    ajout intitulé job02

--- jobs/jour01/job02/index.php
+++ jobs/jour01/job02/index.php
@@ -1,3 +1,8 @@
+Dans le dossier runtrack2/jour01/job02, créez un fichier index.php.
+Dans ce fichier, faites afficher deux fois “Hello LaPlateforme!”
+Commentez chacune de ces lignes d'une façon différente (une ligne, un bloc)
+
+
 <?php
 
 /*$str = "Hello LaPlateforme !" ; 
