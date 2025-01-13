471d0af483f30f53660af49e21ea5fe101e84fa3
sandraMasson-F <sandra.bacot@laplateforme.io>
2025-01-13 10:35:08 +0100

    fin job01

---
+++ jobs/jour01/job01/index.php
@@ -0,0 +1,22 @@
+<?php
+
+$str = "LaPlateforme" ;
+$str2 = "Vive" ;
+$str3 = "!" ;
+
+
+echo  $str2," ", $str," ", $str3;?>  <br><br>
+
+<?php
+$val = 6 + 4;
+
+echo $val;
+
+?><br><br>
+
+<?php
+
+$myBool = false;
+
+echo $myBool
+?>
