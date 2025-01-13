f6bc60e6e30d877b1e6711dfd447d5d5ed004520
sandraMasson-F <sandra.bacot@laplateforme.io>
2025-01-13 11:35:11 +0100

    fin job03

--- jobs/jour01/job03/index.php
+++ jobs/jour01/job03/index.php
@@ -1,4 +1,14 @@
 <?php
+// Dans le dossier runtrack2/jour01/job03, créez un fichier index.php.
+// En PHP, il existe différents types de variables. Créez des variables de types
+// primitifs (boolean, entier, chaîne de caractères, nombre à virgule flottante) et affectez-y des valeurs.
+
+// À l’aide de php, générer un tableau html qui contient dans son header trois colonnes (type, nom, valeur) et dans son body, une ligne pour chacune des variables et de leurs informations.
+?>
+
+
+
+<?php
 
 $boolean = true ; 
 $string = "Hello LaPlateforme !" ;
@@ -17,12 +27,39 @@ $float = 15.12;
   </head>
   <body>
   
-
-  
+  <table>
+  <thead>
+    <tr>
+      <th scope="col">Type</th>
+      <th scope="col">Nom</th>
+      <th scope="col">Valeur</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">Booléen</th>
+      <td>$boolean</td>
+      <td><?php echo $boolean;?></td>
+    </tr>
+    <tr>
+      <th scope="row">Chaine de caractères</th>
+      <td>$string</td>
+      <td><?php echo $string;?></td>
+    </tr>
+    <tr>
+      <th scope="row">Nombre entier</th>
+      <td>$integer</td>
+      <td><?php echo $integer;?></td>
+    </tr>
+    <tr>
+      <th scope="row">Nombre flottant</th>
+      <td>$float</td>
+      <td><?php echo $float;?></td>
+    </tr>
+  </tbody>
+  </table>
+ 
   </body>
 </html>
 
-<?php
 
-
-?>
