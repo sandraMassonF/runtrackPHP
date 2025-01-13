69626ac3ff9194d615b72abfe4d0a8079b2abdcd
sandraMasson-F <sandra.bacot@laplateforme.io>
2025-01-13 11:22:40 +0100

    ajout intitulé job01

--- jobs/jour01/job01/index.php
+++ jobs/jour01/job01/index.php
@@ -1,3 +1,13 @@
+Dans le fichier runtrack2/jour01/job01, créer un fichier index.php.
+Dans ce fichier, créez une variable nommée str. Affectez-lui la valeur
+“LaPlateforme”. Affichez le contenu de la variable str.
+Créez une variable str2 contenant “Vive” et une variable str3 contenant “!”.
+Affichez “Vive LaPlateforme !” en concaténant l'ensemble de ces variables.
+Créez une variable nommée val. Affectez à cette variable la valeur 6.
+Affichez le contenu de la variable. Ajoutez 4 à cette variable. Affichez à nouveau le contenu.
+Créez une variable myBool, affectez à cette variable true, affichez le contenu de la variable. Affectez false à la variable et affichez à nouveau le contenu.
+
+
 <?php
 
 $str = "LaPlateforme" ;
---
+++ jobs/jour01/job03/index.php
@@ -0,0 +1,28 @@
+<?php
+
+$boolean = true ; 
+$string = "Hello LaPlateforme !" ;
+$integer = 15;
+$float = 15.12;
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Tableau Test</title>
+    <link rel="stylesheet" href="./style.css" />
+  </head>
+  <body>
+  
+
+  
+  </body>
+</html>
+
+<?php
+
+
+?>
