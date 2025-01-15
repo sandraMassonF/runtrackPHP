dd7db26410e9ce58e8f63544b42eecdbe6e7e8fc
sandraMasson-F <sandra.bacot@laplateforme.io>
2025-01-15 12:14:54 +0100

    fin job04

---
+++ jobs/jour03/job04/index.php
@@ -0,0 +1,12 @@
+<!-- Créez une string nommée str et affectez-y “Dans l'espace, personne ne vous entend crier”. Créez un algorithme qui parcourt, compte et affiche le nombre de caractères présents dans cette chaîne.-->
+
+
+<?php
+$str = "Dans l'espace, personne ne vous entend crier";
+
+for($i=0; $i < strlen($str); $i++) {   
+        
+    }
+echo $i;
+
+
