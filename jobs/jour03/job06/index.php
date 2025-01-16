569fd3d82e4c009b80a154fdd8995d3acc05f6b2
sandraMasson-F <sandra.bacot@laplateforme.io>
2025-01-16 09:35:20 +0100

    fin job06

---
+++ jobs/jour03/job06/index.php
@@ -0,0 +1,22 @@
+<!-- Créez une chaîne de caractères str qui contient “Les choses que l'on Possède finissent par nous posséder.”. Créez un algorithme qui parcourt et écrit cette chaine à l’envers.-->
+
+
+<?php
+
+$str = "Les choses que l'on possede finissent par nous posseder";
+
+// $strReverse = strrev($str);
+// echo $strReverse . "<br>";
+
+$strToTab = str_split($str);
+
+$count = 0;
+
+foreach ($strToTab as $value) {
+    $count++;
+    $reversed = array_reverse($strToTab);
+}
+
+for ($i = 0; $i < $count; $i++) {
+    echo $reversed[$i];
+}
