8a8295b5f45c7cd90484c4fa03a9e2305fef73d3
sandraMasson-F <sandra.bacot@laplateforme.io>
2025-01-16 16:40:10 +0100

    jour04

---
+++ jobs/jour04/job01/index.php
@@ -0,0 +1,31 @@
+<!--Développez un algorithme qui affiche le nombre d’arguments $_GET.
+Tips : Pour tester votre code, créez un formulaire html de type GET.-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <form action="index.php" method="GET">
+    <label>Votre nom :</label>
+    <input name="nom" id="nom" type="text" />
+<br><br>
+    <label>Votre prénom :</label>
+    <input name="prenom" id="prenom" type="text" />
+<br><br>
+    <label>Votre âge :</label>
+    <input name="age" id="age" type="number" />
+<br><br>
+    <button type="submit">Valider</button>
+   </form> 
+
+
+</body>
+</html>
+
+
+
+
---
+++ jobs/jour04/job01/test.php
@@ -0,0 +1,15 @@
+<?php
+
+$aDeviner = 150;
+?>
+
+<?php 
+?>
+
+
+<form action="index.php" method="GET">
+    <label>Votre nom :</label>
+    <input name="chiffre" id="chiffre" type="number" placeholder="entre 0 et 1000" />
+    <button type="submit">Deviner</button>
+
+</form>
