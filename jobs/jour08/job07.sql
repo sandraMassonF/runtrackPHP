b12301ca9230880820c4c90cdfca6ddc9809bf3b
sandraMasson-F <sandra.bacot@laplateforme.io>
2025-01-22 11:51:19 +0100

    fin job07

---
+++ jobs/jour08/job07.sql
@@ -0,0 +1,3 @@
+-- Écrivez dans le fichier “job07.sql” une requête permettant de sélectionner l’ensemble des informations des étudiants qui ont plus de 18 ans.
+
+SELECT * FROM `etudiants` WHERE TIMESTAMPDIFF(YEAR, naissance, NOW()) >= 18;
