dd7ef72fb47f402ad7aca6b047ac89a226e8820e
sandraMasson-F <sandra.bacot@laplateforme.io>
2025-01-22 16:28:42 +0100

    fin job18!

---
+++ jobs/jour08/job18!.sql
@@ -0,0 +1,3 @@
+-- Écrivez dans le fichier “job18.sql” une requête permettant de récupérer le nom de l'étage ayant la salle avec la plus grande capacité (et afficher aussi le nom de cette salle ainsi que sa capacité). Dans ce résultat, la colonne “nom” de la salle doit être renommée en “Biggest Room”.
+
+SELECT salle.nom AS 'Biggest Room', MAX(salle.capacite), etage.nom FROM salle INNER JOIN etage ON salle.id_etage = etage.id;
