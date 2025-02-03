982ad61025a258b5aa8def1e955095622034175b
sandraMasson-F <sandra.bacot@laplateforme.io>
2025-02-03 16:31:59 +0100

    fin job04

---
+++ les_classes/jour1/job04/index.php
@@ -0,0 +1,56 @@
+<!-- Créer une classe nommée “Point” avec les attributs “x” et “y” correspondant aux coordonnées horizontales et verticales du point. Les deux attributs doivent être initialisés dans le constructeur.
+
+La classe “Point” doit posséder les méthodes suivantes :
+➔ “afficherLesPoints()” qui affiche les coordonnées des points.
+➔ “afficherX()” et “afficherY()” qui affiche respectivement x et y.
+➔ “changerX()” et “changerY()” qui change la valeur des attributs x et y. -->
+
+<?php 
+
+class Point {
+    public $x;
+    public $y;
+
+
+    public function __construct(?int $x,?int $y) {
+        $this->x= $x;
+        $this->y= $y;
+
+    }
+
+    public function afficherLesPoints(){
+        $afficher = "Les coordonées des points sont : $this->x et $this->y <br>";
+        return $afficher;
+    }
+
+    public function afficherX(){
+        $afficheX = "La valeur de X est : $this->x <br>";
+        return $afficheX;
+    }
+    public function afficherY(){
+        $afficheY = "La valeur de Y est : $this->y <br>";
+        return $afficheY;
+    }
+
+    public function changerX($newX){
+        $this->x = $newX;
+        $changementX = "La nouvelle variable de X est : $this->x <br>";
+        return $changementX;
+    }
+    public function changerY($newY){
+        $this->y = $newY;
+        $changementY = "La nouvelle variable de Y est : $this->y <br>";
+        return $changementY;
+    }
+
+};
+
+$affichagePoints = new Point(5,6);
+echo $affichagePoints->afficherLesPoints();
+echo $affichagePoints->afficherX();
+echo $affichagePoints->afficherY();
+echo $affichagePoints->changerX(12);
+echo $affichagePoints->changerY(52);
+echo $affichagePoints->afficherX();
+
+
---
+++ les_classes/jour1/job05/index.php
@@ -0,0 +1,12 @@
+<!-- Créez une classe “Animal” avec un attribut “age” initialisé à zéro et “prenom” initialisé vide dans son constructeur.
+Instancier un objet “Animal” et afficher l’attribut “age”. Créer une méthode “vieillir()” qui ajoute 1 à l'âge de l’animal. Faire vieillir votre animal et afficher son âge.
+Résultat attendu : L'age de l'animal 0 an
+                   L'age de l'animal 1 an
+
+Créer une méthode “nommer()” qui prend en paramètre le nom de l’animal.
+Nommer votre animal et afficher en console le nom de l’animal.
+Résultat attendu : L'animal se nomme Luna -->
+
+
+<?php 
+
