0ca225fcb94f76b911d4e712c2b28af501d0b2b6
sandraMasson-F <sandra.bacot@laplateforme.io>
2025-02-04 10:33:40 +0100

    fin job01

--- les_classes/jour2/job01.php
+++ les_classes/jour2/job01.php
@@ -2,4 +2,47 @@
 Créer des assesseurs et mutateurs afin de pouvoir afficher et modifier les attributs de la classe.
 Créer un rectangle avec les valeurs suivantes : longueur 10 et largeur 5. Changer la valeur de la longueur et de la largeur et vérifier que les modifications soient bien prises en compte. -->
 
+<?php 
+
+class Rectangle{
+
+    private $longueur = 0;
+    private $largeur = 0;
+
+
+    public function __construct($longueur, $largeur){
+        $this->longueur = $longueur;
+        $this->largeur = $largeur;
+    }
+
+    public function getLongeur(){
+        return $this->longueur;
+
+    }
+    public function getLargeur(){
+        return $this->largeur;
+        
+    }
+
+    public function setLongeur($newLongeur){
+        $this->longueur = $newLongeur;
+    }
+    public function setLargeur($newLargeur){
+        $this->largeur = $newLargeur;
+    }
+
+};
+
+$rectangle = new Rectangle(10,5);
+
+echo $rectangle->getLongeur();
+echo "<br>";
+echo $rectangle->getLargeur();
+echo "<br>";
+$rectangle->setLongeur(15);
+$rectangle->setLargeur(25);
+echo "<br>";
+echo $rectangle->getLongeur();
+echo "<br>";
+echo $rectangle->getLargeur();
 
