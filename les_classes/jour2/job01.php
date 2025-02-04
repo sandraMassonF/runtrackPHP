b0001b85796eb847ac05228dd33bf129672d389e
sandraMasson-F <sandra.bacot@laplateforme.io>
2025-02-04 10:09:21 +0100

    réorganisation des fichiers + création dossier jour2

---
+++ les_classes/jour2/job01.php
@@ -0,0 +1,5 @@
+<!-- Créer une classe “Rectangle” avec des attributs privés, “longueur” et “largeur” initialisés dans le constructeur.
+Créer des assesseurs et mutateurs afin de pouvoir afficher et modifier les attributs de la classe.
+Créer un rectangle avec les valeurs suivantes : longueur 10 et largeur 5. Changer la valeur de la longueur et de la largeur et vérifier que les modifications soient bien prises en compte. -->
+
+
